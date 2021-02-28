391457d53c9d0755c2b108d26c80aadad02139b6
Youness Srhir <Ysrhir@hotmail.fr>
2021-02-28 15:37:50 +0100

    looks better

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 import Dropzone from "./dropzone/Dropzone";
 import Process from "./process/Process";
 import ReactDOM from 'react-dom';
+import {ButtonGroup,Button} from '@material-ui/core';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
-
+const [currentTab, setCurrentTab] = useState(["2","1"])
+useEffect(()=>{
+  document.getElementById(currentTab[0]).style.color="rgba(19, 13, 13, 0.8)";
+  document.getElementById(currentTab[0]).style.backgroundColor="#D9C5B2"
+  document.getElementById(currentTab[1]).style.color="lightgray";
+  document.getElementById(currentTab[1]).style.backgroundColor="#34312D";
+ 
+},[currentTab])
+const changeTab =(e)=>{
+ setCurrentTab([currentTab[1],e.target.id])
+}
   return (
-    <div>
-      
       <div className="content">
       <div className="header">
         <p>Logo</p>
@@ -21,22 +30,25 @@ function App() {
       </div>
       <div className="main">
         <BrowserRouter>
-        <div>
+      
           <nav className="navbar">
-          <Link to="/" >Convert pdf to txt</Link>
-          <Link to="/modeling">Topic modeling</Link>
+           
+          <ButtonGroup  disableElevation variant="contained" color="black" id="boutons__nav">
+         <Link id="1" to="/" onClick={changeTab}> Convert pdf to txt</Link> 
+         <Link id="2" to="/modeling"   onClick={changeTab}>Topic modeling</Link>
+          </ButtonGroup>
+          
+          
           </nav>
+          
           <div className="main-route-place">
             <Route exact path="/" component={() => <Dropzone download={true} path={"/process"} type={"pdf"} /> }/>
             <Route path="/modeling" component={Process} />
-           
-            
-          </div>
-        </div>
+            </div>
       </BrowserRouter>
       </div>
       </div>
-    </div>
+   
   );
 }
 
